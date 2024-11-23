ad02c921dccf15f452d06d6b29e996326c715e4f
Ebram-Y-assaad <ebrambotros84@gmail.com>
2024-11-23 11:01:15 +0200

    reduxtoolkit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import {store} from './app/store.js'
+import { fetchUsers } from "./features/users/userSlice.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fetchPosts } from "./features/posts/postsSlice.jsx";
+
+store.dispatch(fetchPosts());
+store.dispatch(fetchUsers());
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+      <Route path='/*' element={<App/>}/>
+      </Routes>
+      </Router>
+    </Provider>
+  </StrictMode>
+);
